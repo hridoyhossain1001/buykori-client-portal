635f6890b210886f7759bdb2f7db61a58a07d6b9
hridoyhossain100100 <134464434+hridoyhossain100100@users.noreply.github.com>
2026-05-16 00:45:37 +0600

    Update migration down_revision to match Heroku database

--- migrations/versions/6f95f5ca1ac2_add_composite_index_to_eventlog.py
+++ migrations/versions/6f95f5ca1ac2_add_composite_index_to_eventlog.py
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '6f95f5ca1ac2'
-down_revision: Union[str, None] = '9b1c2d3e4f56'
+down_revision: Union[str, None] = '2346a5d9e53f'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
